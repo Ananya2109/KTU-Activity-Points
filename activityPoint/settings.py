6710b65a3d56c0bbbc207e7f6eadeb646d99544c
ananyadaniel <ananyadaniels@gmail.com>
2023-05-15 00:44:29 +0530

    integrated postgresql using supabase

--- activityPoint/settings.py
+++ activityPoint/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'activityPoint.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'postgres',
+       'USER': 'postgres',
+       'PASSWORD': 'uHGVu1mSqk9JooWh',
+       'HOST': 'db.dbijjlzzaaikuhkrlpbj.supabase.co',
+       'PORT': '5432',
+   }
 }
 
 
